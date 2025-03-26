d349e697554cc19139a462e480524c6abe1478f8
Abhay <abhaykoli214@gmail.com>
2025-03-26 23:57:57 +0530

    chnages

--- src/common/config.js
+++ src/common/config.js
@@ -1,9 +1,9 @@
 const config = {
   //:local host
-  API_URL: "http://localhost:8080",
+  // API_URL: "http://localhost:8080",
   // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   // live host
-  // API_URL: "https://real-esate-oghre.ondigitalocean.app",
+  API_URL: "https://real-esate-oghre.ondigitalocean.app",
 };
 
 export default config;
